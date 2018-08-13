9eb09dc3605e92f8d6e21ff73572fe27292bf59e
NAVEEN SHARMA <naveens007@users.noreply.github.com>
2018-08-13 15:49:30 +0530

    prime.py | The main code

--- prime.py
+++ prime.py
@@ -27,22 +27,18 @@
 #   Please contribute to this by reporting bugs and making it better....
 #   This code may have some MINOR BUGS....still very stable.
 #   Does not print colored output in IDLE and may print GARBLED characters!
-#   WARNING: Do not use as a module!
 
-#   Code Version: 1.0.3 THETA Θ
+#   Code Version: 1.0.4 THETA Θ
 #   Type "version" , "yelp" or "authors" for more info
 
 """
 
 
 
-#__name__ = '__main__' <= Testing purposes only
-
-
 import math # import math module
 
 authors = "Hemanya Sharma & Naveen Sharma"
-version = "1.0.3 THETA"
+version = "1.0.4 THETA"
 yelp = "The Prime Number Checker (TPNC) is a SUPER-EFFICIENT Prime-number finder that uses an efficient mechanism of log (N) where N is the number of digits in the input."
 
 minimum = 1
@@ -55,10 +51,11 @@ class colors: # Gets a bit dark in iPython
     NEG1 = '\033[3m'
 
 
+
 # isPrime - Predicate function that tells whether the giver number is prime or not...
 # It uses an efficient mechanism of log (N) where N is the number of digits in the input.
-def isPrime (num):
 
+def isPrime (num):
    num = abs (num) # Get abs (absolute) value
    if (num <= minimum):
        print (colors.GREEN + "The number" , num , "is neither a prime number nor a composite number.\n" , colors.ENDC)
@@ -72,6 +69,7 @@ def isPrime (num):
 
    print (colors.GREEN + "The number",num,"is a prime number.\n" , colors.ENDC)
    return True # Return False if the number is prime
+
 
 
 
@@ -90,14 +88,7 @@ if (__name__ == '__main__'):
     mains ()
 
 else:
-    if (__name__ != '__main__'):
-
-        print ("An error occurred: __name__ of" , type(__name__) , "equals" , colors.NEG1 + __name__ , colors.ENDC)
-        __name__ = '__main__'
-        print ("Fixed the problem...",colors.GREEN + "OK\n" , colors.ENDC)
-    else:
-        print ("An unknown error occurred: Unknown! *_*\n")
-
+    print ("Running code as a module...\n")
 
 
 ##########################
